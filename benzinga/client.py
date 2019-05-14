94c9185936117bc72a2559a074a375ef48c6ae2c
rtiwary <tiwaryronit@gmail.com>
2019-05-14 14:54:26 -0400

    added chart

--- benzinga/client.py
+++ benzinga/client.py
@@ -9,32 +9,43 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 
+class BenzingaError():
+    pass
+
 class Benzinga:
     def __init__(self, api_token):
         self.token = api_token 
+        self.headers = {'accept': 'application/json'}
         
     def __url_call__(self, part_one, part_two): #Private Method to modify requests calls 
         url_string = "https://api.benzinga.com/api/v2/" + part_one + "/" + part_two + "/"
         return url_string
-        
+
+    def alternative__url_call__(self, part_one): #Private Method to modify requests calls 
+        url_string = "https://data.benzinga.com/rest/v2/" + part_one
+        return url_string
+
     def ratings(self, company_ticker, start_date, end_date = "", interval = "1D"):
         if not end_date:
             end_date = dt.date.today().strftime('%Y-%m-%d') #if the user doesn't enter a end date, the current date is the default date
-        ratingsUrl = self.__url_call__("calendar", "ratings")
-        headers = {'accept': 'application/json'}
+        url = self.__url_call__("calendar", "ratings")
         params = {'token': token, 'parameters[date_from]': start_date, 'parameters[date_to]': end_date, 'parameters[tickers]': company_ticker}
-        ratings = requests.get(ratingsUrl, headers=headers, params=params)
+        ratings = requests.get(url, headers=self.headers, params=params) 
         return ratings.json()
     
     def financials(self, company_ticker, start_date, end_date = "", interval = "1D"):
         print("need to be here so doesn't give me indentation error")
 
 
+    def chart(self, ticker, date_range, interval):
+        url = self.alternative__url_call__("chart")
+        params = {'symbol': ticker.upper(), 'from': date_range, 'interval': interval}
+        chart = requests.get(url, headers=self.headers, params=params)
+        return chart.json()
 
 if __name__ == '__main__':
     token = "899efcbfda344e089b23589cbddac62b"
     sample_run = Benzinga(token)
-    print(sample_run.ratings("AAPL", "2019-01-01"))
-    
+    print(json.dumps(sample_run.chart('AAPL', '1Y', '1W'), indent=4))
     
 
