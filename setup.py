bf639989c11db440874935982cef76e951ef5ff6
Muhammad Afzal <afzalmaa@umich.edu>
2019-06-12 08:56:39 -0400

    check

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from distutils.core import setup
 setup(
-        name='Benzinga Data Client',
+        name='Benzinga Python Client',
         version='0.5',
         description='Python Client Library for Benzinga Data',
         author='Benzinga Developers',
         author_email='dev@benzinga.com',
         url='https://github.com/Benzinga/benzinga-python-client',
-        packages= ["Benzinga Data Client"],
+        packages= ["Benzinga Python Client"],
         install_requires=['requests'] )
