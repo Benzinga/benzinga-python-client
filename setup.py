8e25da1a6dfa161bc88d33e7a811dc43851a315a
Muhammad Afzal <afzalmaa@umich.edu>
2019-06-13 12:26:17 -0400

    new version setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 setuptools.setup(
         name='benzinga',
-        version='0.95',
+        version='1.0.0',
         description='Python Client Library for Benzinga Data',
         author='Benzinga Developers',
         author_email='dev@benzinga.com',
