bfd49462a60dc6f76c332b8d3c26eb66acb156f6
Muhammad Afzal <afzalmaa@umich.edu>
2019-07-17 11:14:20 -0400

    movers

--- benzinga/tests.py
+++ benzinga/tests.py
@@ -4,10 +4,19 @@ import news_data
 token = "899efcbfda344e089b23589cbddac62b"
 api_key = "22f84f867c5746fd92ef8e13f5835c02"
 newapikey = "54b595f497164e0499409ca93342e394"
+tomkey = "54b595f497164e0499409ca93342e394"
+
+data = financial_data.Benzinga(tomkey)
+result = data.movers(period_from="-1w")
+print(data.output(result))
 
 
-data = financial_data.Benzinga(api_key)
-result = data.price_history(company_tickers="GNLN", date_from= "2019-07-01", date_to= "2019-07-04")
 
-print(result)
+
+
+
+
+
+
+
 
