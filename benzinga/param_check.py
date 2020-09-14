a41c3a56685ac9d10d697bde7da0167d59cab32e
smugilani <umargilani@benzinga.com>
2020-09-14 15:41:38 -0400

    version version 1.16

--- benzinga/param_check.py
+++ benzinga/param_check.py
@@ -52,7 +52,7 @@ class Param_Check:
 
     def delayed_quote_check(self, dict):
         param_type = {
-            'token': self.stri,
+            'apikey': self.stri,
             "symbols": self.stri,
             "isin": self.stri,
             "cik": self.stri
